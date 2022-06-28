96782dac24b952726ac912b8d4a5d384033fb3b4
Joel Maria <joelmsanto@gmail.com>
2022-06-28 14:26:40 -0400

    add cart list item

---
+++ packages/CartItem/src/CartItem.stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View,StyleSheet,ScrollView, Image, Text } from 'react-native';
+
+import {getMainPhoto} from "@jmsstudiosinc/commons";
+import Chip from '../../ReactNativePaper/components/Chip/Chip';
+import CartItem from './CartItem';
+import mockData from "./mockData.json";
+
+export default {
+  title: 'packages/CartItem',
+};
+
+export const CustomDescription = () => (
+  <ScrollView>
+    {mockData.map(product => <CartItem 
+    id={product.id} 
+    uuid={product.uuid} 
+    photo={product.photos?.[0]} 
+    title={product.title} 
+    description={product.description} 
+    price={product.price} 
+    quantity={product.quantity} 
+    isOutofStock={product.isOutofStock} />
+  )}
+  </ScrollView>
+);
+
+
+
