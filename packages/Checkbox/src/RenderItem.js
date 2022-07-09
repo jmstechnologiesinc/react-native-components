d9a2f5316dd18b3321bbf471ceb408705e71fa6b
yerloveraaa <yerloveraaa@gmail.com>
2022-07-09 17:11:41 -0400

    Add checkbox type or circle

--- packages/Checkbox/src/RenderItem.js
+++ packages/Checkbox/src/RenderItem.js
@@ -1,5 +1,8 @@
 import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
+
+import useState from 'storybook-addon-state';
+
 import {FIELD_TYPES} from '@jmsstudiosinc/commons';
 import {
   Paragraph,
@@ -8,6 +11,8 @@ import {
 } from '@jmsstudiosinc/react-native-paper';
 
 const renderCheckboxRadio = item => {
+  const [checkedNormal, setCheckedNormal] = useState(true);
+
   return (
     <View
       style={[
@@ -16,11 +21,15 @@ const renderCheckboxRadio = item => {
           backgroundColor: '#fff',
         },
       ]}>
-      <TouchableRipple onPress={() => console.log('hey')}>
+      <TouchableRipple onPress={() => setCheckedNormal(!checkedNormal)}>
         <View style={styles.row}>
           <Paragraph>{item.title}</Paragraph>
           <View pointerEvents="none">
-            <Checkbox status={'checked'} />
+            <Checkbox
+              status={checkedNormal ? 'checked' : 'unchecked'}
+              type={item.type}
+              disabled={item.isDisabled}
+            />
           </View>
         </View>
       </TouchableRipple>
@@ -46,7 +55,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingVertical: 8,
   },
 
   row: {
