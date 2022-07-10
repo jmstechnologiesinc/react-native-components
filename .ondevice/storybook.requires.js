851f2a5a5d1a635385cb783b16a0a89781eb6bcd
yerloveraaa <yerloveraaa@gmail.com>
2022-07-09 20:43:54 -0400

    Add Tabs Wrapper

--- .ondevice/storybook.requires.js
+++ .ondevice/storybook.requires.js
@@ -5,19 +5,19 @@ import {
   addDecorator,
   addParameters,
   addArgsEnhancer,
-} from "@storybook/react-native";
+} from '@storybook/react-native';
 
-import "@storybook/addon-ondevice-notes/register";
-import "@storybook/addon-ondevice-controls/register";
-import "@storybook/addon-ondevice-backgrounds/register";
-import "@storybook/addon-ondevice-actions/register";
+import '@storybook/addon-ondevice-notes/register';
+import '@storybook/addon-ondevice-controls/register';
+import '@storybook/addon-ondevice-backgrounds/register';
+import '@storybook/addon-ondevice-actions/register';
 
-import { argsEnhancers } from "@storybook/addon-actions/dist/modern/preset/addArgs";
+import {argsEnhancers} from '@storybook/addon-actions/dist/modern/preset/addArgs';
 
-import { decorators, parameters } from "./preview";
+import {decorators, parameters} from './preview';
 
 if (decorators) {
-  decorators.forEach((decorator) => addDecorator(decorator));
+  decorators.forEach(decorator => addDecorator(decorator));
 }
 
 if (parameters) {
@@ -26,17 +26,18 @@ if (parameters) {
 
 // temporary fix for https://github.com/storybookjs/react-native/issues/327 whilst the issue is investigated
 try {
-  argsEnhancers.forEach((enhancer) => addArgsEnhancer(enhancer));
+  argsEnhancers.forEach(enhancer => addArgsEnhancer(enhancer));
 } catch {}
 
 const getStories = () => {
   return [
-    require("../packages/AttributeGroupList/src/AttributeGroupList.stories.js"),
-    require("../packages/Breakdown/src/Breakdown.stories.js"),
-    require("../packages/CartList/src/CartList.stories.js"),
-    require("../packages/Checkbox/src/Checkbox.stories.js"),
-    require("../packages/ProductList/src/ProductList.stories.js"),
-    require("../packages/VendorList/src/VendorList.stories.js"),
+    require('../packages/AttributeGroupList/src/AttributeGroupList.stories.js'),
+    require('../packages/Breakdown/src/Breakdown.stories.js'),
+    require('../packages/CartList/src/CartList.stories.js'),
+    require('../packages/Checkbox/src/Checkbox.stories.js'),
+    require('../packages/HLTabsWrapper/src/index.stories.js'),
+    require('../packages/ProductList/src/ProductList.stories.js'),
+    require('../packages/VendorList/src/VendorList.stories.js'),
   ];
 };
 
