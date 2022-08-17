884cae3f5072a962737d859716d6708ac26d79fd
yerloveraaa <yerloveraaa@gmail.com>
2022-08-17 10:09:48 -0400

    Update package and remove comments

--- packages/components/lib/VendorList/VendorItem.js
+++ packages/components/lib/VendorList/VendorItem.js
@@ -1,45 +1,46 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Card, TouchableRipple} from '@jmsstudiosinc/react-native-paper';
-// import {useNavigation} from '@react-navigation/native';
+import { StyleSheet, View } from 'react-native';
+import { Card, TouchableRipple } from '@jmsstudiosinc/react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 
-const renderVendorItem = ({item}) => {
-  // const navigation = useNavigation();
+const renderVendorItem = ({ item }) => {
+    const navigation = useNavigation();
 
-  return (
-    <View style={[styles.container]} contentContainerStyle={styles.content}>
-      <TouchableRipple
-        onPress={() =>
-          navigation.navigate('SingleVendor', {
-            vendor: item,
-          })
-        }>
-        <Card style={styles.card} mode="elevated">
-          <Card.Cover source={{uri: item.photos}} />
-          <Card.Title
-            title={item.title}
-            subtitle={`${item.formattedPub} - ${item.formattedHitDistance}`}
-            titleVariant="headlineSmall"
-            subtitleVariant="bodyLarge"
-          />
-        </Card>
-      </TouchableRipple>
-    </View>
-  );
+    return (
+        <View style={[styles.container]} contentContainerStyle={styles.content}>
+            <TouchableRipple
+                onPress={() =>
+                    navigation.navigate('SingleVendor', {
+                        vendor: item,
+                    })
+                }
+            >
+                <Card style={styles.card} mode="elevated">
+                    <Card.Cover source={{ uri: item.photos }} />
+                    <Card.Title
+                        title={item.title}
+                        subtitle={`${item.formattedPub} - ${item.formattedHitDistance}`}
+                        titleVariant="headlineSmall"
+                        subtitleVariant="bodyLarge"
+                    />
+                </Card>
+            </TouchableRipple>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  content: {
-    padding: 4,
-  },
-  card: {
-    marginVertical: 4,
-    marginHorizontal: 8,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    content: {
+        padding: 4,
+    },
+    card: {
+        marginVertical: 4,
+        marginHorizontal: 8,
+    },
 });
 
 export default renderVendorItem;
--- packages/components/src/ProductList/SectionList.js
+++ packages/components/src/ProductList/SectionList.js
@@ -1,52 +1,39 @@
 import React from 'react';
-import {SectionList as NativeSectionList, Animated} from 'react-native';
+import { SectionList as NativeSectionList, Animated } from 'react-native';
 
 const AnimatedSectionList = Animated.createAnimatedComponent(NativeSectionList);
 
-import {ProductListHeader, ProductListItem} from './ProductList';
+import { ProductListHeader, ProductListItem } from './ProductList';
 
 const SectionList = ({
-  sections,
-  onPress,
-  sectionListRef,
-  blockUpdateIndexRef,
-  setCurrentIdex,
-  currentIndex,
-  ...props
+    sections,
+    onPress,
+    sectionListRef,
+    blockUpdateIndexRef,
+    setCurrentIdex,
+    currentIndex,
+    ...props
 }) => {
-  return (
-    <AnimatedSectionList
-      {...props}
-      sections={sections}
-      // ref={ref => (sectionListRef.current = ref)}
-      // onMomentumScrollEnd={() => (blockUpdateIndexRef.current = false)}
-      // showsVerticalScrollIndicator={true}
-      // onViewableItemsChanged={({viewableItems}) => {
-      //   if (!blockUpdateIndexRef.current && viewableItems[0]) {
-      //     const {index} = viewableItems[0].section;
-      //     if (currentIndex !== index) {
-      //       setCurrentIdex(index);
-      //     }
-      //   }
-      // }}
-      renderSectionHeader={({section: {title}}) => (
-        <ProductListHeader title={title} />
-      )}
-      renderItem={({item}) => (
-        <ProductListItem
-          id={item.id}
-          uuid={item.uuid}
-          photo={item.photos?.[0]}
-          title={item.title}
-          description={item.description}
-          price={item.price}
-          quantity={item.quantity}
-          isOutofStock={item.isOutofStock}
-          onPress={onPress}
+    return (
+        <AnimatedSectionList
+            {...props}
+            sections={sections}
+            renderSectionHeader={({ section: { title } }) => <ProductListHeader title={title} />}
+            renderItem={({ item }) => (
+                <ProductListItem
+                    id={item.id}
+                    uuid={item.uuid}
+                    photo={item.photos?.[0]}
+                    title={item.title}
+                    description={item.description}
+                    price={item.price}
+                    quantity={item.quantity}
+                    isOutofStock={item.isOutofStock}
+                    onPress={onPress}
+                />
+            )}
         />
-      )}
-    />
-  );
+    );
 };
 
 export default SectionList;
