7c8c94ce63bf0d80f9683f98c5db68856eae3d2d
Joel Maria <joelmsanto@gmail.com>
2022-09-16 16:05:04 -0400

    delete lib

--- packages/components/lib/PhotoGallery/PhotoGallery.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import PhotoGallery from './PhotoGallery';
-
-export default {
-  title: 'packages/PhotoGallery',
-};
-
-export const ProductGallery = () => <PhotoGallery photos={["./wrecked-ship.jpg", "./wrecked-ship.jpg"]} />;
