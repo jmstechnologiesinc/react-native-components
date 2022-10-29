30892b7a5b7884ffa828e44db0c957b77981b79b
Angel <yerlover@gmail.com>
2022-10-28 21:47:43 -0400

    Add md tokens to list

--- packages/components/src/List/ListImage.js
+++ packages/components/src/List/ListImage.js
@@ -2,45 +2,42 @@ import React from 'react';
 
 import { StyleSheet, Image, Platform } from 'react-native';
 
-import {List, MD3LightTheme, Text} from '@jmsstudiosinc/react-native-paper';
+import { List, MD3LightTheme, Text } from '@jmsstudiosinc/react-native-paper';
+import { moderateScale } from 'react-native-size-matters';
 
-const ListImage = ({
-    src,
-    title,
-    description,
-    right,
-    ...props
-}) => {
-  const renderImage = src ? () => <Image source={src} style={styles.image}  /> : null;
-  const renderTitle = ({selectable, titleEllipsizeMode, color}) => (
-    <Text
-      selectable={selectable}
-      ellipsizeMode={titleEllipsizeMode}
-      numberOfLines={1}
-      variant={"bodyLarge"}
-      style={{ color, paddingLeft: MD3LightTheme.margin / 2, marginTop: -5}}>
-      {title}
-    </Text>
-  );
-  
-  return (
-      <List.Item
-        {...props}
-        title={renderTitle}
-        description={description}
-        left={renderImage}
-        right={right}
-        style={{paddingLeft: 0}}
-        descriptionStyle={{paddingLeft: MD3LightTheme.margin / 2}}
-        itemStyle={{marginVertical: 0, justifyContent: true ? "flex-start" : "center"}} />
-  )
-}
+const ListImage = ({ src, title, description, right, ...props }) => {
+    const renderImage = src ? () => <Image source={src} style={styles.image} /> : null;
+    const renderTitle = ({ selectable, titleEllipsizeMode, color }) => (
+        <Text
+            selectable={selectable}
+            ellipsizeMode={titleEllipsizeMode}
+            numberOfLines={1}
+            variant={'bodyLarge'}
+            style={{ color, paddingLeft: MD3LightTheme.spacing.xxxSmall, marginTop: moderateScale(-5) }}
+        >
+            {title}
+        </Text>
+    );
+
+    return (
+        <List.Item
+            {...props}
+            title={renderTitle}
+            description={description}
+            left={renderImage}
+            right={right}
+            style={{ paddingLeft: 0 }}
+            descriptionStyle={{ paddingLeft: MD3LightTheme.spacing.xxxSmall }}
+            itemStyle={{ marginVertical: 0, justifyContent: true ? 'flex-start' : 'center' }}
+        />
+    );
+};
 
 const styles = StyleSheet.create({
-  image: {
-    width: 100,
-    height: 56
-  }
+    image: {
+        width: moderateScale(100),
+        height: MD3LightTheme.spacing.xxXlarge,
+    },
 });
 
 export default ListImage;
