a7e486dcf508237295cb74f90747957c2540a738
Angel <yerlover@gmail.com>
2022-10-31 14:11:45 -0400

    add moderate scale to tabs

--- packages/components/src/Tabs/TabsItem.js
+++ packages/components/src/Tabs/TabsItem.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
 import { Button, MD3LightTheme } from '@jmsstudiosinc/react-native-paper';
+import { moderateScale } from 'react-native-size-matters';
 
-const TabsItem = ({  
-    title, 
-    isSelected, 
-    onPress, 
-}) => <Button
-    compact
-    textColor={isSelected ? MD3LightTheme.colors.primary : MD3LightTheme.colors.onSurfaceVariant}
-    style={{
-        borderRadius: 1,
-        ...(isSelected && {
-            borderColor: MD3LightTheme.colors.primary,
-            borderBottomWidth: 2,
-        }),
-    }}
-    onPress={onPress}>
+const TabsItem = ({ title, isSelected, onPress }) => (
+    <Button
+        compact
+        textColor={isSelected ? MD3LightTheme.colors.primary : MD3LightTheme.colors.onSurfaceVariant}
+        style={{
+            borderRadius: moderateScale(1),
+            ...(isSelected && {
+                borderColor: MD3LightTheme.colors.primary,
+                borderBottomWidth: moderateScale(2),
+            }),
+        }}
+        onPress={onPress}
+    >
         {title}
-</Button>;
+    </Button>
+);
 
 export default TabsItem;
