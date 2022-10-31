7db985daa0183ce31a1558eab097c05e304b0124
Angel <yerlover@gmail.com>
2022-10-31 14:04:29 -0400

    add md toke to screen wrapper

--- packages/components/src/ScreenWrapper/ScreenWrapperContainer.js
+++ packages/components/src/ScreenWrapper/ScreenWrapperContainer.js
@@ -1,33 +1,38 @@
 import * as React from 'react';
-import { View} from 'react-native';
+import { View } from 'react-native';
 import { MD3LightTheme } from '@jmsstudiosinc/react-native-paper';
 
 const containerPaddings = {
-  top: MD3LightTheme.margin / 2,
-  bottom: MD3LightTheme.margin,
-  left: MD3LightTheme.margin,
-  right: MD3LightTheme.margin
-}
+    top: MD3LightTheme.spacing.xSmall / 2,
+    bottom: MD3LightTheme.spacing.xSmall,
+    left: MD3LightTheme.spacing.xSmall,
+    right: MD3LightTheme.spacing.xSmall,
+};
 
 export const ScreenWrapperContainer = ({
-  withPaddingVertical = true,
-  withPaddingHorizontal = true,
-  paddingTop = containerPaddings.top,
-  paddingBottom = containerPaddings.bottom,
-  paddingLeft = containerPaddings.left,
-  paddingRight = containerPaddings.right,
-  style,
-  children
+    withPaddingVertical = true,
+    withPaddingHorizontal = true,
+    paddingTop = containerPaddings.top,
+    paddingBottom = containerPaddings.bottom,
+    paddingLeft = containerPaddings.left,
+    paddingRight = containerPaddings.right,
+    style,
+    children,
 }) => (
-  <View style={[{
-    flex: 1, 
-    ...(withPaddingVertical && { paddingTop, paddingBottom }), 
-    ...(withPaddingHorizontal && { paddingLeft, paddingRight })
-    }, style]}>
-    {children}
-  </View>
-)
+    <View
+        style={[
+            {
+                flex: 1,
+                ...(withPaddingVertical && { paddingTop, paddingBottom }),
+                ...(withPaddingHorizontal && { paddingLeft, paddingRight }),
+            },
+            style,
+        ]}
+    >
+        {children}
+    </View>
+);
 
-export {containerPaddings}
+export { containerPaddings };
 
-export default ScreenWrapperContainer
+export default ScreenWrapperContainer;
