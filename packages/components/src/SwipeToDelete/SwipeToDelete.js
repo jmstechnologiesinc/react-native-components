a426692e8a700678c373ec4bb29aaf4e6a53cef3
Angel <yerlover@gmail.com>
2022-10-31 14:09:12 -0400

    add md toke to swipte delete

--- packages/components/src/SwipeToDelete/SwipeToDelete.js
+++ packages/components/src/SwipeToDelete/SwipeToDelete.js
@@ -7,24 +7,32 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 import { Button } from '@jmsstudiosinc/react-native-paper';
 
 import { MD3LightTheme } from '@jmsstudiosinc/react-native-paper';
+import { moderateScale } from 'react-native-size-matters';
 
 const SwipeToDelete = ({ children, onDelete }) => {
     const renderRightActions = () => (
         <Button onPress={onDelete}>
             <Animated.View style={[styles.deleteBox]}>
-                <MaterialCommunityIcons name="trash-can-outline" color={'white'} size={30} />
+                <MaterialCommunityIcons
+                    name="trash-can-outline"
+                    color={MD3LightTheme.colors.background}
+                    size={MD3LightTheme.spacing.medium}
+                />
             </Animated.View>
         </Button>
     );
 
-    return <Swipeable
-        renderRightActions={renderRightActions}
-        overshootRight={false}
-        friction={2}
-        leftThreshold={30}
-        rightThreshold={40}>
-        {children}
-    </Swipeable>
+    return (
+        <Swipeable
+            renderRightActions={renderRightActions}
+            overshootRight={false}
+            friction={2}
+            leftThreshold={30}
+            rightThreshold={40}
+        >
+            {children}
+        </Swipeable>
+    );
 };
 
 const styles = StyleSheet.create({
@@ -32,10 +40,10 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: MD3LightTheme.colors.error,
         justifyContent: 'center',
-        width: 90,
+        width: moderateScale(90),
         flexDirection: 'row',
         alignItems: 'center',
-    }
+    },
 });
 
 export default SwipeToDelete;
