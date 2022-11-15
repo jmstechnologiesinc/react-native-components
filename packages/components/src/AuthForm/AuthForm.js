a631615926d9bfe0871d9cdc529e5cceeff6b405
Angel <yerlover@gmail.com>
2022-11-14 22:55:38 -0400

    Add form personal info

--- packages/components/src/AuthForm/AuthForm.js
+++ packages/components/src/AuthForm/AuthForm.js
@@ -1,1 +1,3 @@
 export { default as EmailPassword } from './AuthFormEmailPassword';
+export { default as PersonInfo } from './FormPersonInfo';
+export { default as DriverInfo } from './FormDriverInfo';
---
+++ packages/components/src/AuthForm/FormPersonInfo.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { TextInput } from '@jmsstudiosinc/react-native-paper';
+import ScreenWrapper from '../ScreenWrapper/ScreenWrapper';
+
+const FormPersonInfo = ({
+    title,
+    labelFirstName,
+    firstName,
+    inputActionHandler,
+    labelLastName,
+    lastName,
+    labelPhone,
+    phone,
+}) => {
+    return (
+        <>
+            <ScreenWrapper.Section title={title}>
+                <TextInput
+                    mode="outlined"
+                    label={labelFirstName}
+                    value={firstName}
+                    onChangeText={(text) => inputActionHandler('firstName', text)}
+                />
+            </ScreenWrapper.Section>
+            <ScreenWrapper.Section>
+                <TextInput
+                    mode="outlined"
+                    label={labelLastName}
+                    value={lastName}
+                    onChangeText={(text) => inputActionHandler('lastName', text)}
+                />
+            </ScreenWrapper.Section>
+            <ScreenWrapper.Section>
+                <TextInput
+                    mode="outlined"
+                    label={labelPhone}
+                    value={phone}
+                    onChangeText={(text) => inputActionHandler('phone', text)}
+                />
+            </ScreenWrapper.Section>
+        </>
+    );
+};
+
+export default FormPersonInfo;
