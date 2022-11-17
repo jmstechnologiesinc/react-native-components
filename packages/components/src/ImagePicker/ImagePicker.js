b7f29e2a3a6a792be427202c51698af81d930980
Angel <yerlover@gmail.com>
2022-11-17 14:44:50 -0400

    Add image picker

---
+++ packages/components/src/ImagePicker/ImagePicker.js
@@ -0,0 +1,106 @@
+import { Linking, PermissionsAndroid } from 'react-native';
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+
+const oneTakePhoto = () => {
+    launchCamera(
+        {
+            mediaType: 'photo',
+            quality: 0.5,
+        },
+        (response) => {
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                alert(response.error);
+                console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+            } else {
+                console.log(response);
+            }
+        }
+    );
+};
+
+const chooseFromGallery = (setProfilePictureFile) => {
+    launchImageLibrary(
+        {
+            mediaType: 'photo',
+            quality: 0.5,
+        },
+        (response) => {
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                alert(response.error);
+                console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+            } else {
+               setProfilePictureFile(response?.assets[0]);
+            }
+        }
+    );
+};
+
+const onPressFromGallery = async (setProfilePictureFile) => {
+    if (Platform.OS === 'android') {
+        const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA);
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            chooseFromGallery();
+        } else {
+            Alert.alert(
+                IMLocalized('Camera permission denied'),
+                IMLocalized(
+                    'To have access to the camera you must enable the camera permission in your application settings'
+                ),
+                [
+                    {
+                        text: IMLocalized('Cancel'),
+                        onPress: () => console.log('Cancel Pressed'),
+                        style: 'cancel',
+                    },
+                    {
+                        text: IMLocalized('Go to Settings'),
+                        onPress: () => Linking.openSettings(),
+                    },
+                ],
+                { cancelable: false }
+            );
+        }
+    } else {
+        chooseFromGallery(setProfilePictureFile);
+    }
+};
+
+const onPressFromCamara = async () => {
+    if (Platform.OS === 'android') {
+        const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA);
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            oneTakePhoto();
+        } else {
+            Alert.alert(
+                IMLocalized('Camera permission denied'),
+                IMLocalized(
+                    'To have access to the camera you must enable the camera permission in your application settings'
+                ),
+                [
+                    {
+                        text: IMLocalized('Cancel'),
+                        onPress: () => console.log('Cancel Pressed'),
+                        style: 'cancel',
+                    },
+                    {
+                        text: IMLocalized('Go to Settings'),
+                        onPress: () => Linking.openSettings(),
+                    },
+                ],
+                { cancelable: false }
+            );
+        }
+    } else {
+        oneTakePhoto();
+    }
+};
+
+export { onPressFromGallery, onPressFromCamara };
