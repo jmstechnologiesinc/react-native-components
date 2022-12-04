ba3abe04c620f3d295b32845d36e26771c84fb94
Angel <yerlover@gmail.com>
2022-12-04 11:57:36 -0400

    add condition to display title credentials

--- packages/components/src/AuthForm/AuthFormEmailPassword.js
+++ packages/components/src/AuthForm/AuthFormEmailPassword.js
@@ -14,12 +14,13 @@ const AuthFormEmailPassword = ({
     passwordConfirm,
     resetPassword = false,
     onPasswordReset,
+    titleCredentials = false
 }) => {
     const [isTextSecureEntry, setIsTextSecureEntry] = useState(true);
 
     return (
         <>
-            <ScreenWrapper.Section title={localized('Contact Details')}>
+            <ScreenWrapper.Section title={titleCredentials ? localized('Credentials'): null}>
                 <TextInput
                     label={localized('Email')}
                     value={email}
