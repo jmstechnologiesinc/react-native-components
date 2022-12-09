f791a023fe48a6c2701d233ade9959e864ccaf4c
Angel <yerlover@gmail.com>
2022-12-09 14:52:21 -0400

    Refactor

--- packages/components/src/ImagePicker/ImagePickerAPI.js
+++ packages/components/src/ImagePicker/ImagePickerAPI.js
@@ -57,42 +57,47 @@ class ImagePickerAPI {
         );
     }
 
-    async takePhoto(onChange, actionSheetRef) {
-        await checkAndAskForPermissionCamara()
-            .then(() => launchCamera({ mediaType: 'photo', quality: 0.5 }))
-            .then((response) => {
-                if (response.didCancel) {
-                    console.log('User cancelled image picker');
-                } else if (response.error) {
-                    Alert(response.error);
-                    console.log('ImagePicker Error: ', response.error);
-                } else if (response.customButton) {
-                    console.log('User tapped custom button: ', response.customButton);
-                } else {
-                    onChange(response?.assets[0]);
-                    actionSheetRef.current.hide();
-                }
-            })
-            .catch(() => this.catchPermissionStatusCamara());
+
+
+    takePhoto() {
+        return new Promise((resolve) => {
+            checkAndAskForPermissionCamara()
+                .then(() => launchCamera({ mediaType: 'photo', quality: 0.5 }))
+                .then((response) => {
+                    if (response.didCancel) {
+                        console.log('User cancelled image picker');
+                    } else if (response.error) {
+                        Alert(response.error);
+                        console.log('ImagePicker Error: ', response.error);
+                    } else if (response.customButton) {
+                        console.log('User tapped custom button: ', response.customButton);
+                    } else {
+                        resolve(response?.assets[0]);
+                    }
+                })
+                .catch(() => this.catchPermissionStatusCamara());
+        });
     }
 
-    async chooseFromLibrary(onChange, actionSheetRef) {
-        await checkAndAskForPermissionMediaLibrary();
-        launchImageLibrary({ mediaType: 'photo', quality: 0.5 })
-            .then((response) => {
-                if (response.didCancel) {
-                    console.log('User cancelled image picker');
-                } else if (response.error) {
-                    Alert(response.error);
-                    console.log('ImagePicker Error: ', response.error);
-                } else if (response.customButton) {
-                    console.log('User tapped custom button: ', response.customButton);
-                } else {
-                    onChange(response?.assets[0]);
-                    actionSheetRef.current.hide();
-                }
-            })
-            .catch(() => this.catchPermissionStatusPhotos());
+
+    chooseFromLibrary() {
+        return new Promise((resolve) => {
+            checkAndAskForPermissionMediaLibrary()
+                .then(() => launchImageLibrary({ mediaType: 'photo', quality: 0.5 }))
+                .then((response) => {
+                    if (response.didCancel) {
+                        console.log('User cancelled image picker');
+                    } else if (response.error) {
+                        Alert(response.error);
+                        console.log('ImagePicker Error: ', response.error);
+                    } else if (response.customButton) {
+                        console.log('User tapped custom button: ', response.customButton);
+                    } else {
+                        resolve(response?.assets[0]);
+                    }
+                })
+                .catch(() => this.catchPermissionStatusPhotos());
+        });
     }
 }
 
