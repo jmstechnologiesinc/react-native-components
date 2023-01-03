681f812143ee4f7ec5122c5d9bfebd0bd62ae293
Angel <yerlover@gmail.com>
2023-01-03 14:51:50 -0400

    Fix

--- packages/components/src/List/CheckRadio.js
+++ packages/components/src/List/CheckRadio.js
@@ -5,38 +5,31 @@ import { View } from 'react-native';
 import { List, RadioButton, Checkbox } from '@jmsstudiosinc/react-native-paper';
 import { MetaBadged } from '../List/List';
 
-const CheckRadio = ({ 
-    title, 
-    description, 
-    metadata, 
-    isDisabled, 
-    isChecked, 
-    onPress, 
-    variant="radio",
-    ...props 
-}) => (
+const CheckRadio = ({ title, description, metadata, isDisabled, isChecked, onPress, variant = 'radio', ...props }) => (
     <List.Item
         title={title}
         description={description}
         onPress={() => onPress?.(!isChecked)}
         disabled={isDisabled}
-        left={(props) => variant === "radio" ? (
-            <View style={props.style}>
-                <RadioButton.Android
-                    status={isChecked ? 'checked' : 'unchecked'}
-                    disabled={isDisabled}
-                    onPress={() => onPress?.(!isChecked)}
-                />
-            </View>
-        ) : (
-            <View style={props.style}>
-                <Checkbox
-                    status={isChecked ? 'checked' : 'unchecked'}
-                    disabled={isDisabled}
-                    onPress={() => onPress?.(!isChecked)}
-                />
-            </View>
-        )}
+        left={(props) =>
+            variant === 'radio' ? (
+                <View style={props.style}>
+                    <RadioButton.Android
+                        status={isChecked ? 'checked' : 'unchecked'}
+                        disabled={isDisabled}
+                        onPress={() => onPress?.(!isChecked)}
+                    />
+                </View>
+            ) : (
+                <View style={props.style}>
+                    <Checkbox.Android
+                        status={isChecked ? 'checked' : 'unchecked'}
+                        disabled={isDisabled}
+                        onPress={() => onPress?.(!isChecked)}
+                    />
+                </View>
+            )
+        }
         right={() => <MetaBadged title={metadata} />}
         {...props}
     />
