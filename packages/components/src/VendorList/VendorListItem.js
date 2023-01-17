1b2cdcb1bcbb23b3666e5d5cc5846c748d310f7b
Joel Maria <joelmsanto@gmail.com>
2023-01-17 00:53:32 -0500

    rename deliveryOnly/pickupOnly to delivery/pickup

--- packages/components/src/VendorList/VendorListItem.js
+++ packages/components/src/VendorList/VendorListItem.js
@@ -6,11 +6,11 @@ import { VENDOR_INDUSTRIES_MAPPING } from '@jmsstudiosinc/vendor';
 import { interpunct, getMainPhoto } from '@jmsstudiosinc/commons';
 
 const VendorListItem = ({ item, onPress }) => (
-    <Card mode="elevated" onPress={() => onPress(item)}>
+    <Card mode="contained" onPress={() => onPress(item)}>
         <Card.Cover source={{ uri: getMainPhoto(item.photos) }} />
         <Card.Title
             title={item.title}
-            subtitle={interpunct([item.formattedPub, item.formattedHitDistance])}
+            subtitle={interpunct([item.formattedFulfillmentMethod, item.formattedHitDistance])}
             titleNumberOfLines={0}
             titleVariant="headlineSmall"
             subtitleVariant="bodyLarge"
