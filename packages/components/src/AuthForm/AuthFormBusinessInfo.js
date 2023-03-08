35836f1bc2cea26519d39876b227673ad3d5665c
joel maria <jmstechnologiesinc@joels-MacBook-Pro.local>
2023-03-08 17:30:45 -0500

    refactor

--- packages/components/src/AuthForm/AuthFormBusinessInfo.js
+++ packages/components/src/AuthForm/AuthFormBusinessInfo.js
@@ -1,34 +1,54 @@
 import React from 'react';
 
-import { TextInput } from '@jmsstudiosinc/react-native-paper';
+import { TextInput,Caption } from '@jmsstudiosinc/react-native-paper';
+
 import ScreenWrapper from '../ScreenWrapper/ScreenWrapper';
 import { localized } from '../Localization/Localization';
 
-const AuthFormBusinessInfo = ({ storeTitle, storeAddress, industry, placeholder, inputActionHandler }) => {
+const AuthFormBusinessInfo = ({ title, fullAddress, line2, industry, website, industryPlaceholder, inputActionHandler }) => {
     return (
         <>
             <ScreenWrapper.Section title={localized('Business Details')}>
                 <TextInput
                     mode="outlined"
                     label={localized('Store Name')}
-                    value={storeTitle}
-                    onChangeText={(text) => inputActionHandler('storeTitle', text)}
+                    value={title}
+                    onChangeText={(text) => inputActionHandler('title', text)}
                 />
             </ScreenWrapper.Section>
             <ScreenWrapper.Section>
                 <TextInput
                     mode="outlined"
-                    label={localized('Full Store Address')}
-                    value={storeAddress}
-                    placeholder={placeholder}
-                    onChangeText={(text) => inputActionHandler('storeAddress', text)}
+                    label={localized('Store Address')}
+                    value={fullAddress}
+                    onChangeText={(text) => inputActionHandler('fullAddress', text)}
                 />
+            </ScreenWrapper.Section>
+            <ScreenWrapper.Section>
+                <TextInput
+                    mode="outlined"
+                    label={localized('Floor / Suite')}
+                    value={line2}
+                    onChangeText={(text) => inputActionHandler('line2', text)}
+                />
+            </ScreenWrapper.Section>
+            <ScreenWrapper.Section>
+                <TextInput
+                    mode="outlined"
+                    label={localized('Website')}
+                    value={website}
+                    onChangeText={(text) => inputActionHandler('website', text)}
+                />
+                <Caption>
+                    {localized('If you don’t have a website, enter a social media page, LinkedIn, or other relevant link.')}
+                </Caption>
             </ScreenWrapper.Section>
             <ScreenWrapper.Section>
                 <TextInput
                     mode="outlined"
                     label={localized('Industry')}
                     value={industry}
+                    placeholder={industryPlaceholder}
                     onChangeText={(text) => inputActionHandler('industry', text)}
                 />
             </ScreenWrapper.Section>
