e25e42740bf366e0190627bf8a6f3f83398195b1
actcarrasco <actcarrasco@gmail.com>
2023-03-24 16:58:25 -0400

    Add scrollview

--- packages/components/src/AuthForm/SelectIndustries.js
+++ packages/components/src/AuthForm/SelectIndustries.js
@@ -1,24 +1,14 @@
-
-import React, { useRef } from 'react'
-
-import {
-    TextInput,
-    RadioButton,
-    List,
-    Button,
-
-} from "@jmsstudiosinc/react-native-paper";
+import React, { useRef } from 'react';
+import { ScrollView } from 'react-native';
+import { TextInput, RadioButton, List, Button } from '@jmsstudiosinc/react-native-paper';
 
 import { interpunct } from '@jmsstudiosinc/commons';
-import ActionSheet from 'react-native-actions-sheet';
+import ActionSheet, { useScrollHandlers } from 'react-native-actions-sheet';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-import {
-    styles as JMSStyles,
-} from '@jmsstudiosinc/react-native-components';
-
+import { styles as JMSStyles, localized } from '@jmsstudiosinc/react-native-components';
 
 export default function SelectIndustries({ placeholder, OPTIONS, onUpdateValue, inputActionHandler }) {
-   
     const actionSheetRef = useRef();
 
     const showActionSheet = () => {
@@ -29,28 +19,23 @@ export default function SelectIndustries({ placeholder, OPTIONS, onUpdateValue, 
         actionSheetRef.current.hide();
     };
 
-    const selectedItems = OPTIONS.filter(item => item.selected === true);
-    const isSelect = selectedItems.map((item) => item.item)
-    const itemSelected = interpunct(isSelect)
+    const scrollHandlers = useScrollHandlers('scrollview-1', actionSheetRef);
 
-    
+    const selectedItems = OPTIONS.filter((item) => item.selected === true);
+    const isSelect = selectedItems.map((item) => item.item);
+    const itemSelected = interpunct(isSelect);
+
+    const insets = useSafeAreaInsets();
 
     return (
         <>
-
             <TextInput
                 editable={false}
                 multiline={true}
                 value={itemSelected}
                 placeholder={placeholder}
                 mode="outlined"
-                right={
-                    <TextInput.Icon
-                        icon={"chevron-down-circle-outline"}
-                        onPress={() => showActionSheet()}
-                    />
-
-                }
+                right={<TextInput.Icon icon={'chevron-down-circle-outline'} onPress={() => showActionSheet()} />}
             />
 
             <ActionSheet
@@ -59,35 +44,40 @@ export default function SelectIndustries({ placeholder, OPTIONS, onUpdateValue, 
                 drawUnderStatusBar={true}
                 springOffset={50}
                 defaultOverlayOpacity={0.3}
+                containerStyle={{
+                    paddingTop: insets.top,
+                    paddingBottom: insets.bottom,
+                }}
             >
-
-                <List.Section>
-                    {OPTIONS.map(({ item, selected }, index) => (
-                        <List.Item
-                            key={index}
-                            title={item}
-                            onPress={() => onUpdateValue(index, selected)}
-                            left={(props) => <RadioButton.Android
-                                {...props}
-                                status={selected ? "checked" : "unchecked"}
+                <ScrollView {...scrollHandlers}>
+                    <List.Section>
+                        {OPTIONS.map(({ item, selected }, index) => (
+                            <List.Item
+                                key={index}
+                                title={item}
                                 onPress={() => onUpdateValue(index, selected)}
-                            />}
-                        />
-                    ))}
-
-                    <Button
-                        mode="contained"
-                        style={JMSStyles.fba}
-                        onPress={() => {
-                            inputActionHandler('industry', isSelect)
-                            hideActionSheet()
-                        }}
-
-                    >SAVE</Button>
-                </List.Section>
-
+                                left={(props) => (
+                                    <RadioButton.Android
+                                        {...props}
+                                        status={selected ? 'checked' : 'unchecked'}
+                                        onPress={() => onUpdateValue(index, selected)}
+                                    />
+                                )}
+                            />
+                        ))}
+                    </List.Section>
+                </ScrollView>
+                <Button
+                    mode="contained"
+                    style={JMSStyles.fba}
+                    onPress={() => {
+                        inputActionHandler('industry', isSelect);
+                        hideActionSheet();
+                    }}
+                >
+                    {localized('SAVE')}
+                </Button>
             </ActionSheet>
         </>
-
-    )
+    );
 }
--- packages/components/src/AuthForm/ultis.js
+++ packages/components/src/AuthForm/ultis.js
@@ -1,22 +1,62 @@
- 
- import { localized } from '../Localization/Localization';
- import { VENDOR_INDUSTRIES_MAPPING } from '@jmsstudiosinc/vendor';
+import { localized } from '../Localization/Localization';
+import { VENDOR_INDUSTRIES_MAPPING } from '@jmsstudiosinc/vendor';
 
- export const OPTIONS = [
-        {
-            item: localized(VENDOR_INDUSTRIES_MAPPING.Restaurant.title),
-            selected: true
-        },
-        {
-            item: localized(VENDOR_INDUSTRIES_MAPPING.GroceryGourmet.title),
-            selected: false
-        },
-        {
-            item: localized(VENDOR_INDUSTRIES_MAPPING.Liquor.title),
-            selected: false
-        },
-        {
-            item: localized(VENDOR_INDUSTRIES_MAPPING.Pharmacy.title),
-            selected: false
-        },
-    ]
+export const OPTIONS = [
+    {
+        item: localized(VENDOR_INDUSTRIES_MAPPING.Restaurant.title),
+        selected: true,
+    },
+    {
+        item: localized(VENDOR_INDUSTRIES_MAPPING.GroceryGourmet.title),
+        selected: false,
+    },
+    {
+        item: localized(VENDOR_INDUSTRIES_MAPPING.Liquor.title),
+        selected: false,
+    },
+    {
+        item: localized(VENDOR_INDUSTRIES_MAPPING.Pharmacy.title),
+        selected: false,
+    },
+    {
+        item: localized(VENDOR_INDUSTRIES_MAPPING.VideoGames.title),
+        selected: false,
+    },
+    {
+        item: localized(VENDOR_INDUSTRIES_MAPPING.LuggageTravelGear.title),
+        selected: false,
+    },
+    {
+        item: localized(VENDOR_INDUSTRIES_MAPPING.ToysGames.title),
+        selected: false,
+    },
+
+    {
+        item: localized(VENDOR_INDUSTRIES_MAPPING.GardenOutdoor.title),
+        selected: false,
+    },
+    {
+        item: localized(VENDOR_INDUSTRIES_MAPPING.Electronics.title),
+        selected: false,
+    },
+    {
+        item: localized(VENDOR_INDUSTRIES_MAPPING.Computers.title),
+        selected: false,
+    },
+    {
+        item: localized(VENDOR_INDUSTRIES_MAPPING.CellPhones.title),
+        selected: false,
+    },
+    {
+        item: localized(VENDOR_INDUSTRIES_MAPPING.Books.title),
+        selected: false,
+    },
+    {
+        item: localized(VENDOR_INDUSTRIES_MAPPING.BeautyPersonalCare.title),
+        selected: false,
+    },
+    {
+        item: localized(VENDOR_INDUSTRIES_MAPPING.Baby.title),
+        selected: false,
+    },
+];
