afbafca49a70e33050b4dcab966ca32c787dcd10
joel maria <jmstechnologiesinc@joels-MacBook-Pro.local>
2023-05-01 01:47:53 -0400

    payment stable

---
+++ packages/components/src/Accounting/Checkout.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Accounting from './Accounting';
+
+import mockData from "./mockData.json"
+
+export default {
+  title: 'packages/Checkout',
+};
+
+export const Fees = () => <Accounting feeList={mockData} />
