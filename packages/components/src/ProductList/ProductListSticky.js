a4f5cfee709cd37bb99adb286c484021245992c2
JMS Technologies <jmstechnologies@JMSs-Air.home>
2023-08-09 16:46:11 -0400

    add view cart button

--- packages/components/src/ProductList/ProductListSticky.js
+++ packages/components/src/ProductList/ProductListSticky.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import { List } from '@jmstechnologiesinc/react-native-paper';
 import { moderateScale } from 'react-native-size-matters';
+import { View } from 'react-native';
 
 import StickySectionList from '../StickySectionList/StickySectionList';
 import ProductListItem from './ProductListItem';
 import { imagekitUrl } from '../utils';
-
 
 const keyExtractor = (productItem) => productItem.id;
 
@@ -17,33 +17,41 @@ const ProductListSticky = ({
     onPress,
     onContentOffsetYScroll,
     contentOffsetY,
+    ButtonCartFAB,
     ...props
 }) => (
-    <StickySectionList
-        {...props}
-        sections={sections}
-           renderSectionHeader={({ section: { title } }) => (
-            <List.Subheader style={{ height: moderateScale(50) }}>{title}</List.Subheader>
-        )}
-        keyExtractor={keyExtractor}
-        onContentOffsetYScroll={onContentOffsetYScroll}
-        contentOffsetY={contentOffsetY}
-        renderItem={({ item }) => (
-            <ProductListItem
-                id={item.id}
-                uuid={item.uuid}
-                photo={imagekitUrl(item.photo)}
-                title={item.title}
-                description={item.description}
-                formattedPrice={item.price.formattedValue}
-                cartQuantity={productItemQuantityMapping?.[item.id]}
-                quantity={item.quantity}
-                isOutofStock={item.isOutofStock}
-                fulfillmentMethodFilter={fulfillmentMethodFilter}
-                onPress={() => onPress(item)}
-            />
-        )}
-    />
+    <>
+        <StickySectionList
+            {...props}
+            sections={sections}
+            renderSectionHeader={({ section: { title } }) => (
+                <List.Subheader style={{ height: moderateScale(50) }}>{title}</List.Subheader>
+            )}
+            keyExtractor={keyExtractor}
+            onContentOffsetYScroll={onContentOffsetYScroll}
+            contentOffsetY={contentOffsetY}
+            renderItem={({ item }) => (
+                <>
+                    <ProductListItem
+                        id={item.id}
+                        uuid={item.uuid}
+                        photo={imagekitUrl(item.photo)}
+                        title={item.title}
+                        description={item.description}
+                        formattedPrice={item.price.formattedValue}
+                        cartQuantity={productItemQuantityMapping?.[item.id]}
+                        quantity={item.quantity}
+                        isOutofStock={item.isOutofStock}
+                        fulfillmentMethodFilter={fulfillmentMethodFilter}
+                        onPress={() => onPress(item)}
+                    />
+                </>
+            )}
+        />
+        <View style={{ bottom: moderateScale(80) }}>
+            <ButtonCartFAB />
+        </View>
+    </>
 );
 
 ProductListSticky.whyDidYouRender = true;
