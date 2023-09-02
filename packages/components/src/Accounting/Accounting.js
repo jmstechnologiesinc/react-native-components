402789333b97a749cd5a54051ad510e883b0a1f7
JMS Technologies <jmstechnologies@JMSs-MacBook-Air.local>
2023-09-02 00:18:24 -0400

    Translate platform commission

--- packages/components/src/Accounting/Accounting.js
+++ packages/components/src/Accounting/Accounting.js
@@ -4,6 +4,23 @@ import { MD3LightTheme } from '@jmstechnologiesinc/react-native-paper';
 import * as JMSList from '../List/List';
 
 import { calculatableAccoutingList } from '@jmstechnologiesinc/cart';
+import { localized } from '../Localization/Localization';
+
+const isTranslatePlatformCommission = (title) => {
+
+const regex = /^platformCommission\((\d+%)\)$/;
+
+const match = title.match(regex);
+
+if (match) {
+  const percentage = match[1];
+  return localized('platformCommission', {percentage})
+} else {
+    return localized(title)
+  
+}
+
+}
 
 const feesListItem = (feeList) => {
     if (!feeList?.length) {
@@ -11,7 +28,6 @@ const feesListItem = (feeList) => {
     }
 
     const results = [];
-
 
     for (const feeItem of calculatableAccoutingList(feeList)) {
         const styles =
@@ -31,7 +47,7 @@ const feesListItem = (feeList) => {
         results.push(
             <JMSList.Item
                 key={feeItem.id}
-                title={feeItem.label}
+                title={isTranslatePlatformCommission(feeItem.label)}
                 description={feeItem.description}
                 metaTitle={feeItem.formattedValue}
                 {...styles}
