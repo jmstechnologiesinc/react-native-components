9e82c87c3bec2e4f9ff0bd2d5f6e3329b87232cb
angel <angel@angels-MBP.home>
2023-11-25 18:41:29 -0400

    Add hight to actionsheet

--- packages/components/src/OptionPicker/OptionPickerActionSheet.js
+++ packages/components/src/OptionPicker/OptionPickerActionSheet.js
@@ -1,36 +1,38 @@
 import React, { useRef, useState } from 'react';
-import { ScrollView } from 'react-native';
-import { Button ,HelperText} from '@jmstechnologiesinc/react-native-paper';
+import { Dimensions, Platform, ScrollView } from 'react-native';
+import { Button, HelperText } from '@jmstechnologiesinc/react-native-paper';
 import ActionSheet, { useScrollHandlers } from 'react-native-actions-sheet';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { ChipList, styles as JMSStyles, localized, ScreenWrapper,} from '@jmstechnologiesinc/react-native-components';
+import { ChipList, styles as JMSStyles, localized, ScreenWrapper } from '@jmstechnologiesinc/react-native-components';
 import NestedOptionPicker from './NestedOptionPicker';
 import { MD3LightTheme } from '@jmstechnologiesinc/react-native-paper/src/styles/themes';
 import ButtonWrapper from '../ButtonWrapper/ButtonWrapper';
 
-function OptionPickerActionSheet({ 
-    isDisabled, 
-    onPress, 
-    options = [], 
+const windowHight = Dimensions.get('window').height;
+
+function OptionPickerActionSheet({
+    isDisabled,
+    onPress,
+    options = [],
     preSelectedOptions = [],
     multiple = true,
     chipListTitle = localized('Categories'),
     helpText,
-    addButtonTitle = localized('pick')
+    addButtonTitle = localized('pick'),
 }) {
     const actionSheetRef = useRef();
     const insets = useSafeAreaInsets();
     const scrollHandlers = useScrollHandlers('scrollview-1', actionSheetRef);
 
     const [selectedOptions, setSelectedOptions] = useState(preSelectedOptions);
-  
+
     const handleRemoveChip = (index) => {
         const optionToRemove = preSelectedOptions[index];
-        setSelectedOptions(prev => prev.filter(option => option.id !== optionToRemove.id));
-        onPress(preSelectedOptions.filter(option => option.id !== optionToRemove.id));
+        setSelectedOptions((prev) => prev.filter((option) => option.id !== optionToRemove.id));
+        onPress(preSelectedOptions.filter((option) => option.id !== optionToRemove.id));
     };
 
-    const selectedTitles = preSelectedOptions.map(option => option.title);
+    const selectedTitles = preSelectedOptions.map((option) => option.title);
 
     const showActionSheet = () => {
         setSelectedOptions(preSelectedOptions);
@@ -38,6 +40,7 @@ function OptionPickerActionSheet({
     };
 
     const hideActionSheet = () => actionSheetRef.current.hide();
+    const actionSheetHight = Platform.OS === 'ios' ? windowHight - 120 : null;
 
     return (
         <>
@@ -47,26 +50,27 @@ function OptionPickerActionSheet({
                     options={selectedTitles}
                     onPress={showActionSheet}
                     onClose={handleRemoveChip}
-                    chipStyle={{marginBottom: MD3LightTheme.spacing.x2}}
+                    chipStyle={{ marginBottom: MD3LightTheme.spacing.x2 }}
                 />
                 <ButtonWrapper
                     title={addButtonTitle}
                     isDisabled={isDisabled}
-                    onPress={showActionSheet}   
-                    style={{marginLeft: 0}}/>
-                {helpText ? (<HelperText padding="none">{helpText}</HelperText>) : null}
+                    onPress={showActionSheet}
+                    style={{ marginLeft: 0 }}
+                />
+                {helpText ? <HelperText padding="none">{helpText}</HelperText> : null}
             </ScreenWrapper.Section>
-           
+
             <ActionSheet
                 ref={actionSheetRef}
                 statusBarTranslucent={true}
-                drawUnderStatusBar={true}
+                drawUnderStatusBar={false}
                 springOffset={50}
                 defaultOverlayOpacity={0.3}
                 gestureEnabled
                 containerStyle={{
-                    paddingTop: insets.top,
                     paddingBottom: insets.bottom,
+                    height: actionSheetHight,
                 }}
             >
                 <ScrollView {...scrollHandlers}>
@@ -75,7 +79,8 @@ function OptionPickerActionSheet({
                         options={options}
                         preSelectedOptions={preSelectedOptions}
                         multiple={multiple}
-                        onPress={setSelectedOptions} />
+                        onPress={setSelectedOptions}
+                    />
                 </ScrollView>
                 <Button
                     mode="outlined"
