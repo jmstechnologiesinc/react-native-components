e981a671125d5fc29f9f8ae620b121df859425e9
jms technologies inc <jmstechnologiesinc@jmss-MacBook-Pro.local>
2024-08-10 22:33:59 -0400

    ufff

--- packages/components/src/Order/usePubNubETA.js
+++ packages/components/src/Order/usePubNubETA.js
@@ -1,16 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
 import PubNub from 'pubnub';
+import Config from 'react-native-config';
 
 import { USER_ROLES } from '@jmstechnologiesinc/user';
 import { ORDER_STATUS } from '@jmstechnologiesinc/order';
 import { pubnubEtaChannelName } from '@jmstechnologiesinc/commons';
 import { useSelector } from 'react-redux';
-
-const PUBNUB = {
-    PUBLISH_KEY: 'pub-c-e618e49b-b38a-4d80-8ee7-0fad9fcda279',
-    SUBSCRIBE_KEY: 'sub-c-c64f4f86-dad8-11eb-8c90-a639cde32e15',
-};
 
 const usePubNubETA = ({ orderId, deliveryMethod, status, role, onPubNub }) => {
     const [etaValue, setEtaValue] = useState(null);
@@ -18,8 +14,8 @@ const usePubNubETA = ({ orderId, deliveryMethod, status, role, onPubNub }) => {
     const user = useSelector((state) => state.auth.user);
 
     const pubnub = new PubNub({
-        publishKey: PUBNUB.PUBLISH_KEY,
-        subscribeKey: PUBNUB.SUBSCRIBE_KEY,
+        publishKey: Config.PUBNUB_PUBLISH_KEY,
+        subscribeKey: Config.PUBNUB_SUBSCRIBE_KEY,
         ssl: true,
         userId: user.id
     });
