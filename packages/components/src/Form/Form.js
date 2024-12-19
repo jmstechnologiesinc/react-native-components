2eba96a30a089d7fd8aaecbed68dddf90ce6bfba
jms technologies inc <jmstechnologiesinc@jmss-MacBook-Pro.local>
2024-12-19 01:12:32 -0500

    ddd

--- packages/components/src/Form/Form.js
+++ packages/components/src/Form/Form.js
@@ -4,7 +4,7 @@ export { default as DriverInfo } from './FormDriverInfo';
 export { default as BusinessInfo } from './FormBusinessInfo';
 export { default as VehicleInfo } from './FormVehicleInfo';
 export { default as PhoneNumber } from './FormPhoneNumber';
-export { default as EmailOrPhoneSwitcher } from './FormEmailOrPhoneSwitcher'
+export { default as EmailOrPhoneSwitcher, LOGIN_OPTIONS } from './FormEmailOrPhoneSwitcher'
 export { default as VerificationCode, PHONE_NUMBER_VERIFICATION_CODE_LENGTH, isValidPhoneNumberVerificationCode } from './FormVerificationCode'
 
 
