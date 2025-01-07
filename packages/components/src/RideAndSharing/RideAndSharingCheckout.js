231366358676f18d90981f86380c5f2720b41b1a
Angel Carrasco <yerlover@gmail.com>
2025-01-07 13:32:02 -0400

    Add action sheet

--- packages/components/src/RideAndSharing/RideAndSharingCheckout.js
+++ packages/components/src/RideAndSharing/RideAndSharingCheckout.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React, { useCallback, useMemo, useRef } from "react";
+import BottomSheet, { BottomSheetFooter, BottomSheetSectionList } from "@gorhom/bottom-sheet";
+import { Divider, FAB, List, MD3LightTheme, Text } from "@jmstechnologiesinc/react-native-paper";
+import TipsFilter from "../TipsFilter/TipsFilter";
+import LocationListItem, { LOCATION_LIST_ITEM } from "../LocationListItem/LocationListItem";
+import { localized } from "../Localization/Localization";
+import ProductListItem from "./ProductListItem";
+import { keyExtractor } from "../CartList/CartList";
+import styles from "../styles";
+import CheckoutSummary from "../CheckoutSummary/CheckoutSummary";
 
-import { SectionList } from 'react-native';
-
-import { Divider, FAB, List, MD3LightTheme } from '@jmstechnologiesinc/react-native-paper';
-import TipsFilter from '../TipsFilter/TipsFilter';
-import LocationListItem, { LOCATION_LIST_ITEM } from '../LocationListItem/LocationListItem';
-import { localized } from '../Localization/Localization';
-import ProductListItem from './ProductListItem';
-import { keyExtractor } from '../CartList/CartList';
-import styles from '../styles';
-
-import CheckoutSummary from '../CheckoutSummary/CheckoutSummary'
-import GeoPositionTracker from '../GeoPositionTracker';
-
+import ScreenWrapper from '../ScreenWrapper/ScreenWrapper'
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 const RideAndSharingCheckout = ({
     originLocationTitle,
     originLocationDescription,
@@ -27,85 +26,124 @@ const RideAndSharingCheckout = ({
     onTipsPercentPress,
     onRequestRidePress,
     RenderPaymentMethod,
-    originLocation,
-    dropoffLocation
+    withBottomInset = true,
+    withTopInset = false,
 }) => {
+    const bottomSheetRef = useRef(null);
+
+    const snapPoints = useMemo(() => ["50%", "70%", "80%", "85", "90", "95", "100%"], []);
+
+    const insets = useSafeAreaInsets();
+
+    const isInsetsBottom = insets.bottom === 0 ? MD3LightTheme.spacing.x4 : insets.bottom;
+
+    const containerStyle = [
+        {
+            paddingTop: withTopInset ? insets.top : 0,
+            paddingBottom: withBottomInset ? isInsetsBottom : 0,
+            paddingLeft: insets.left,
+            paddingRight: insets.left,
+
+        },
+    ]
+
     const listHeaderComponent = () => (
         <>
-            <GeoPositionTracker
-                customerPosition={{
-                    longitude: originLocation.longitude,
-                    latitude: originLocation.latitude
-                }}
-                currentDriverPosition={dropoffLocation}
-                vendorPosition={dropoffLocation}
-            />
             <List.Section title="Shipping Details">
                 <LocationListItem
                     title={originLocationTitle}
                     description={originLocationDescription}
                     variant={LOCATION_LIST_ITEM.currentLocation}
-                    onPress={originLocationOnPress} />
+                    onPress={originLocationOnPress}
+                />
                 <LocationListItem
                     title={dropoffLocationTitle}
                     description={dropoffLocationDescription}
                     variant={LOCATION_LIST_ITEM.currentLocation}
-                    onPress={dropoffLocationOnPress} />
+                    onPress={dropoffLocationOnPress}
+                />
             </List.Section>
-            {
-                RenderPaymentMethod ? <List.Section title={localized("paymentMethod")}>
+            {RenderPaymentMethod ? (
+                <List.Section title={localized("paymentMethod")}>
                     <RenderPaymentMethod />
-                </List.Section> : null
-            }
-
+                </List.Section>
+            ) : null}
         </>
     );
 
-    const ListFooterComponent = () => (
-        <>
+    const listFooterComponent = () => (
+        <View style={{ paddingBottom: insets.bottom }}>
             <TipsFilter
                 options={tipsFilter.options}
                 description={tipsFilter.description}
                 selectedTipsPercentIndex={tipsFilter.selectedTipsPercentIndex}
-                onTipsPercentPress={onTipsPercentPress} />
-
+                onTipsPercentPress={onTipsPercentPress}
+            />
             <CheckoutSummary
                 netFeeList={fees}
-                termsAndConditions={'checkoutTermAndCondition'}
+                termsAndConditions={"checkoutTermAndCondition"}
             />
+        </View>
+    );
 
-        </>
+
+    const renderFooter = useCallback(
+        props => (
+            <BottomSheetFooter {...props}>
+                <ScreenWrapper withScrollView={false} withPaddingHorizontal={false} withBottomInset={true}>
+                    <FAB
+                        label={localized('requestRide')}
+                        variant='secondary'
+                        mode="elevated"
+                        style={[styles.button]}
+                        onPress={onRequestRidePress} />
+                </ScreenWrapper>
+            </BottomSheetFooter>
+        ),
+        []
     );
 
     return (
         <>
-            <SectionList
-                sections={products}
-                keyExtractor={keyExtractor}
-                renderSectionHeader={({ section: { title } }) => (
-                    <List.Subheader >
-                        {title}
-                    </List.Subheader>
-                )}
-                renderItem={({ item }) => (
-                    <ProductListItem
-                        title={item.title}
-                        description={item.description}
-                        price={item.price}
-                        chips={item.chips}
-                        onPress={onItemPress} />
-                )}
-                showsVerticalScrollIndicator={false}
-                showsHorizontalScrollIndicator={false}
-                ListHeaderComponent={listHeaderComponent}
-                ListFooterComponent={ListFooterComponent}
-                stickySectionHeadersEnabled={false}
-            />
-            <FAB label={localized('requestRide')} onPress={onRequestRidePress} style={styles.fba} />
+            <BottomSheet
+                ref={bottomSheetRef}
+                index={3}
+                snapPoints={snapPoints}
+                enablePanDownToClose={false}
+                footerComponent={renderFooter}
+                containerStyle={containerStyle}
+                backgroundStyle={{
+                    flex: 1,
+                    backgroundColor: MD3LightTheme.colors.background,
+                }}
+                handleStyle={{
+                    backgroundColor: MD3LightTheme.colors.background,
+                }}
+            >
+                <BottomSheetSectionList
+                    sections={products}
+                    keyExtractor={keyExtractor}
+                    renderSectionHeader={({ section: { title } }) => (
+                        <List.Subheader>{title}</List.Subheader>
+                    )}
+                    renderItem={({ item }) => (
+                        <ProductListItem
+                            title={item.title}
+                            description={item.description}
+                            price={item.price}
+                            chips={item.chips}
+                            onPress={onItemPress}
+                        />
+                    )}
+                    ListHeaderComponent={listHeaderComponent}
+                    ListFooterComponent={listFooterComponent}
+                    stickySectionHeadersEnabled={false}
+                    showsVerticalScrollIndicator={false}
+                />
+
+            </BottomSheet>
         </>
-    )
+    );
 };
-
-
 
 export default RideAndSharingCheckout;
