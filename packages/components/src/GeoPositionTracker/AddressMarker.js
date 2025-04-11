b61f6b6504cf3d2763185d683deaaa2a0d82c5e1
Angel Carrasco <yerlover@gmail.com>
2025-04-11 15:37:12 -0400

    Fix position tracker

--- packages/components/src/GeoPositionTracker/AddressMarker.js
+++ packages/components/src/GeoPositionTracker/AddressMarker.js
@@ -2,7 +2,6 @@ import { View, StyleSheet } from 'react-native'
 import React from 'react'
 import MapboxGL from '@rnmapbox/maps';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { Text } from '@jmstechnologiesinc/react-native-paper';
 
 import { MD3LightTheme, Text, TouchableRipple } from '@jmstechnologiesinc/react-native-paper';
 
