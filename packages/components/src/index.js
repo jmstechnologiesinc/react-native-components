9dcb75a885dfa719fc07bff8e7f5f66d02b48fed
jms technologies inc <jmstechnologiesinc@jmss-MacBook-Pro.local>
2025-06-07 23:58:00 -0400

    refactor order

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -22,12 +22,9 @@ export { default as PhotoGallery } from './PhotoGallery/PhotoGallery';
 export { default as Accounting } from './Accounting/Accounting';
 export { default as Fast2ImageKit } from './Fast2ImageKit/Fast2ImageKit';
 export { default as ChipList } from './ChipList/ChipList';
-export { default as OrderStatus } from './Order/OrderStatus';
-export { makeLinkingCall, itemSeparator, showActionSheet, hideActionSheet, imagekitUrl, imageKitListImage, imageKitAvatar, imageKitPhotoGalleryMainImage, imageKitPhotoGalleryMainImageLqip, imageKitCard, imageKitCardLqip, imageKitListImagelqip, isPublicUrl } from './utils';
+export { default as Order, formatQuickOrderViewDescription } from './Order';
+export { default as TouchableRippleWrapper } from "./TouchableRippleWrapper/TouchableRippleWrapper"
 export { formatOrder, orderListStatus, groupedOrderListToSectionList, ORDER_LIST_STATUS } from './Order/utils';
-export { default as OrderList } from './OrderList/OrderList';
-export { default as OrderListItem } from './OrderList/OrderListItem';
-export { default as OrderView } from './OrderView/OrderView';
 export { default as IndustryList } from './IndustryList/IndustryList';
 export { default as QuantityButton } from './QuantityButton/QuantityButton';
 export { default as ScreenWrapper } from './ScreenWrapper';
@@ -36,6 +33,7 @@ export { default as SwipeToDelete } from './SwipeToDelete/SwipeToDelete';
 export { default as SideNav } from './SideNav/SideNav';
 export { default as Swipeable } from './SwipeToDelete/SwipeToDelete';
 export { default as ButtonWrapper } from "./ButtonWrapper/ButtonWrapper";
+export { makeLinkingCall, itemSeparator, showActionSheet, hideActionSheet, imagekitUrl, imageKitListImage, imageKitAvatar, imageKitPhotoGalleryMainImage, imageKitPhotoGalleryMainImageLqip, imageKitCard, imageKitCardLqip, imageKitListImagelqip, isPublicUrl } from './utils';
 
 export { default as SocialAuthentication } from "./SocialAuthentication/SocialAuthentication";
 
