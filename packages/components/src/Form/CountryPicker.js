e6ded36c14d956f01c119e4f6229b135a76c8515
Angel carrasco <yerlover@gmail.com>
2025-06-26 00:07:08 -0400

    Fix

--- packages/components/src/Form/CountryPicker.js
+++ packages/components/src/Form/CountryPicker.js
@@ -10,7 +10,6 @@ const WINDOW_HEIGHT = Dimensions.get('window').height;
 
 const CountryPicker = forwardRef(({ data, onSelect }, ref) => {
     const insets = useSafeAreaInsets();
-    const scrollHandlers = useScrollHandlers('scrollview-1', ref);
     const HEADER_HEIGHT = useHeaderHeight();
     const actionSheetHeight = Platform.OS === 'ios' ? WINDOW_HEIGHT - HEADER_HEIGHT : null;
 
@@ -27,7 +26,7 @@ const CountryPicker = forwardRef(({ data, onSelect }, ref) => {
                 height: actionSheetHeight,
             }}
         >
-            <ScrollView {...scrollHandlers}>
+            
                 {data?.map((item) => (
                     <List.Item
                         key={item.key}
@@ -37,7 +36,7 @@ const CountryPicker = forwardRef(({ data, onSelect }, ref) => {
                         left={(props) => <List.Image {...props} variant="flag" source={item.image} defaultImageComponent={true} />}
                     />
                 ))}
-            </ScrollView>
+         
         </ActionSheet>
     );
 });
